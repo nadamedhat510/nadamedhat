a4b5283993ed0dff68294101c6762c2666b45fb8
nadamedhat510 <62850312+nadamedhat510@users.noreply.github.com>
2020-03-30 02:21:37 +0200

    Update DWT.py

--- DWT.py
+++ DWT.py
@@ -4,12 +4,12 @@ def wavelet(data):
 
     coeffs = wavedec(data, 'sym5', level=5)
 
-        #cA5,Cd5,cD4,cD5,cD3, cD2, cD1 = coeffs
+    cA5,Cd5,cD4,cD5,cD3, cD2, cD1 = coeffs
 
     cA5,cD5,cD4,cD3,cD2,cD1=coeffs
 
     A_concate=[cD1,cD2,cD3,cD4,cD5]
 
-    l=A_concate.shape
+    
 
-    print(l)
+   
